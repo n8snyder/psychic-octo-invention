067b10dec16541247a9a0a0199a54e6ba4ae866f
Nate Snyder <natesnyder@Nates-MacBook-Pro.local>
2019-02-27 22:59:48 -0800

    Moved over index page of n8snyder.com. Changed index.html to correctly load the css and js scripts from static.

--- application.py
+++ application.py
@@ -1,11 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
 application = Flask(__name__)
 
 
 @application.route("/")
 def index():
-    return "<h1 style='color:blue'>Hello There!</h1>"
+    # return "<h1 style='color:blue'>Hello There!</h1>"
+    return render_template("index.html")
 
 
 if __name__ == "__main__":
