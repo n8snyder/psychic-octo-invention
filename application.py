9b9ee3c0f153edfc82de4862e6823770d9014fc7
Nate Snyder <n8.snyder@berkeley.edu>
2019-04-17 22:38:20 -0700

    Added route for map app.

--- application.py
+++ application.py
@@ -9,6 +9,11 @@ def index():
     return render_template("index.html")
 
 
+@application.route("/map")
+def map():
+    return render_template("map.html")
+
+
 if __name__ == "__main__":
     application.run(host="0.0.0.0")
 
