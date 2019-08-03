00a69c997038d4b0d6872becae85e00bcf9b5a30
Nate Snyder <n8.snyder@berkeley.edu>
2019-08-03 00:37:07 -0700

    Added sentry
    Requires environment variable SENTRY_DSN.

--- application.py
+++ application.py
@@ -1,4 +1,12 @@
+import sentry_sdk, os
+from sentry_sdk.integrations.flask import FlaskIntegration
+
 from flask import Flask, render_template
+
+sentry_sdk.init(
+    dsn=os.environ['SENTRY_DSN'],
+    integrations=[FlaskIntegration()]
+)
 
 application = Flask(__name__)
 application.templates_auto_reload = True
@@ -13,6 +21,13 @@ def index():
 @application.route("/map")
 def map():
     return render_template("map.html")
+
+
+@application.route('/debug-sentry')
+def trigger_error():
+    foo = 1
+    bar = 0
+    division_by_zero = foo / bar + 1
 
 
 if __name__ == "__main__":
